6278bce7ae7b23ee4ee2948ea8ec014bdf679ea8
indestinee <indestinee@gmail.com>
2019-03-12 15:49:28 +0800

    first commit, version 0.1

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='iutils',
+      version='0.1',
+      description='Some Basic Utils',
+      url='http://github.com/indestinee/iutils',
+      author='indestinee',
+      author_email='indestinee@gmail.com',
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
