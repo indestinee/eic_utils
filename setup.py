3f32a89bf991e68adbbb2dbb86adad0cdb340e36
indestinee <indestinee@gmail.com>
2019-03-12 17:00:03 +0800

    add readme in pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 import setuptools
 
+with open("./eic_utils/README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='eic_utils',
       version='0.3',
-      description='Some Basic Utils',
+      description='basic utils :)',
       url='http://github.com/indestinee/utils',
       author='indestinee',
       author_email='indestinee@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       packages=setuptools.find_packages(),
       classifiers=[
         "Programming Language :: Python :: 3",
