2b849df43590ee470c336d06fd51a4c84b6a1fad
indestinee <indestinee@gmail.com>
2019-03-20 13:24:28 +0800

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("./eic_utils/README.md", "r") as fh:
+with open("./README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='eic_utils',
-      version='0.3',
+      version='0.4',
       description='basic utils :)',
       url='http://github.com/indestinee/utils',
       author='indestinee',
